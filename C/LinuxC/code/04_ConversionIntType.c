28831be830737b2100e3d0d82b3c715d4e2d4032
wei <t104306033@ntut.org.tw>
2019-09-10 00:17:19 +0800

    feat(linuxc/code): practice how to convert between char and int

---
+++ C/LinuxC/code/04_ConversionIntType.c
@@ -0,0 +1,13 @@
+// Practice how to convert between char and int
+
+#include <stdio.h>
+
+int main() {
+    char x = 'a';
+
+    printf("char %c + 1 is equal to char %c\n", x, x+1);
+
+    printf("char %c is also use int to represent, such as int %d\n", x, x);
+
+    return 0;
+}
