d5c92c70b8509025bc4c733b9353d814f6e6a744
wei <t104306033@ntut.org.tw>
2019-11-10 19:34:25 +0800

    feat(linuxc/code): modify 1-6.05_multiplicationTable.c

--- C/LinuxC/code/1-6.05_multiplicationTable.c
+++ C/LinuxC/code/1-6.05_multiplicationTable.c
@@ -5,8 +5,8 @@ int main (void) {
     int i, j;
 
     for (i=1; i<=9; i++) {
-        for (j=1; j<=9; j++)
-            if (j <= i)  printf("%d\t", i*j);
+        for (j=1; j<=i; j++)
+            printf("%d\t", i*j);
         printf("\n");
     }
 
