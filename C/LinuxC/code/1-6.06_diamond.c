a4971f15450af306f22ae996b599fb3ae1abb7c6
wei <t104306033@ntut.org.tw>
2019-11-14 19:51:25 +0800

    feat(linuxc/code): solution 2 of print diamond

--- C/LinuxC/code/1-6.06_diamond.c
+++ C/LinuxC/code/1-6.06_diamond.c
@@ -9,61 +9,111 @@ int isOdd (int number) {
     }
 }
 
-
 void printDiamond (char symbol, int length) {
     auto void sol1 (void);    
-    
-    char space = ' ';
+    auto void sol2 (void);    
+
+    char space = '-';
     int mid = length / 2 + 1;
 
     if (isOdd(length)) {
         sol1();
+        //sol2();
     }
     else {
         printf("Length is even.\n");
     }
 
+    // ----------   ----------   ----------   ----------
+    // concept: using Addition theorem
+    // - - - * - - - 
+    // - - * * * - - 
+    // - * * * * * - 
+    // * * * * * * * 
+    // - * * * * * - 
+    // - - * * * - - 
+    // - - - * - - - 
+    
+    // 1         2        3         4         
+    // - - - *   * - - -  * * * *   * * * *
+    // - - * *   * * - -  - * * *   * * * -
+    // - * * *   * * * -  - - * *   * * - -
+    // * * * *   * * * *  - - - *   * - - -
+
     void sol1 (void) {
         // print upper triangle
         for (int i=1; i<=mid; i++) { 
             for (int j=1; j<=mid; j++) { // print upper-right triangle
-                if (j > (mid-i)) {
+                if (j > (mid-i))
                     printf(" %c", symbol);
-                }
-                else {
+                else
                     printf(" %c", space);
-                }
             }
             for (int j=mid+1; j<=length; j++) { // print upper-left triangle
-
-                if (j < (mid+i)) {
+                if (j < (mid+i))
                     printf(" %c", symbol);
-                }
-                else {
+                else
                     printf(" %c", space);
-                }
             }    
             printf("\n");
         }
 
         // print upper triangle
-        for (int i=mid-1; i>0; i--) { 
+        for (int i=mid-1; i>=1; i--) { 
             for (int j=1; j<=mid; j++) { // print lowwer-right triangle
-                if (j > (mid-i)) {
+                if (j > (mid-i))
                     printf(" %c", symbol);
-                }
-                else {
+                else
                     printf(" %c", space);
-                }
             }
             for (int j=mid+1; j<=length; j++) { // print lowwer-left triangle
-
-                if (j < (mid+i)) {
+                if (j < (mid+i))
                     printf(" %c", symbol);
-                }
-                else {
+                else
                     printf(" %c", space);
-                }
+            }
+            printf("\n");
+        }
+    }
+
+    // ----------   ----------   ----------   ----------
+    // concept: using Subtraction concept
+    // - - - *
+    // - - * * *
+    // - * * * * *
+    // * * * * * * *
+    // - * * * * *
+    // - - * * *
+    // - - - *
+
+    // 1                  2  
+    // * * * *            * * *
+    // * * * * *          * *
+    // * * * * * *        * 
+    // * * * * * * *     
+    // * * * * * *        * 
+    // * * * * *          * * 
+    // * * * *            * * * 
+    
+    void sol2 () {
+        // print upper triangle
+        for(int i=1; i<=mid; i++){
+            for(int j=1; j<(mid+i); j++){
+                if(j > (mid-i))
+                    printf(" %c", symbol);
+                else
+                    printf(" %c", space);
+            }
+            printf("\n");
+        }
+
+        // print lowwer triangle
+        for(int i=mid-1; i>=1; i--){
+            for(int j=1; j<(mid+i); j++){
+                if(j > (mid-i))
+                    printf(" %c", symbol);
+                else
+                    printf(" %c", space);
             }
             printf("\n");
         }
@@ -72,7 +122,7 @@ void printDiamond (char symbol, int length) {
 }
 
 int main (void) {
-    printDiamond('*', 9);
+    printDiamond('*', 7);
 
     return 0;
 }
