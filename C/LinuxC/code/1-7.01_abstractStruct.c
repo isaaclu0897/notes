24e0099946db3ff9ddb4ff44a4c11e7fffe1f3ab
wei <t104306033@ntut.org.tw>
2019-11-21 20:01:25 +0800

    feat(linuxc/code): excerisce abstract struct

---
+++ C/LinuxC/code/1-7.01_abstractStruct.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <math.h>
+
+// struct
+struct complex_struct {
+    double x, y;
+} ;
+
+// property of struct
+double real_part (struct complex_struct z) {
+    return z.x;
+}
+
+double img_part (struct complex_struct z) {
+    return z.y;
+}
+
+double magnitude (struct complex_struct z) {
+    return sqrt(z.x * z.x + z.y * z.y);
+}
+
+double angle (struct complex_struct z) {
+    return atan2(z.y, z.x);
+}
+
+// instance method of struct
+struct complex_struct make_from_real_img (double x, double y) {
+    struct complex_struct z;
+    z.x = x;
+    z.y = y;
+    return z;
+}
+
+struct complex_struct make_from_mag_ang (double r, double A) {
+    struct complex_struct z;
+    z.x = r * cos(A);
+    z.y = r * sin(A);
+    return z;
+}
+
+struct complex_struct add_complex (
+        struct complex_struct z1,
+        struct complex_struct z2
+        ) {
+    return make_from_real_img(
+            real_part(z1) + real_part(z2),
+            img_part(z1) + img_part(z2)
+            );
+}
+
+struct complex_struct sub_complex (
+        struct complex_struct z1,
+        struct complex_struct z2
+        ) {
+    return make_from_real_img(
+            real_part(z1) - real_part(z2),
+            img_part(z1) - img_part(z2)
+            );
+}
+
+static int count = 0;
+void print_struct (struct complex_struct z) {
+    printf("\n=== %d ===\n", ++count);
+    printf("%f %f\n", z.x, z.y);
+    printf("%f %f\n", real_part(z), img_part(z));
+    printf("%f %f\n", magnitude(z), angle(z));
+}
+
+
+int main (void) {
+    struct complex_struct z2, z3, z4, z5;
+    
+    struct complex_struct z1 = { 3.0, 4.0 };
+    print_struct(z1);
+
+    double a = 6.0, b = 8.0;
+    z2 = make_from_real_img(a, b);
+    print_struct(z2);
+
+    z3 = add_complex(z1, z2);
+    print_struct(z3);
+    
+
+
+    return 0;
+}
