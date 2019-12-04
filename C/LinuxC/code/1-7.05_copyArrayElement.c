71cf5809b9c822888a94f51b9bf13bbc7dbe4fa1
wei <t104306033@ntut.org.tw>
2019-12-04 20:11:25 +0800

    feat(linuxc/code): add define into copy element of array

--- C/LinuxC/code/1-7.05_copyArrayElement.c
+++ C/LinuxC/code/1-7.05_copyArrayElement.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
-int main (void) {
-    int a[5] = { 1, 2, 3, 4, 5 };
-    int b[5] = { 0 };
+#define N 5
 
-    for (int i=0; i<5; i++)
+int main (void) {
+    int a[N] = { 1, 2, 3, 4, 5 };
+    int b[N] = { 0 };
+
+    for (int i=0; i<N; i++)
         printf("%d\t", a[i]);
 
     printf("\n");
 
-    for (int i=0; i<5; i++)
+    for (int i=0; i<N; i++)
         printf("%d\t", b[i]);
     printf("\n");
 
-    for (int i=0; i<5; i++)
+    for (int i=0; i<N; i++)
         b[i] = a[i];
 
-    for (int i=0; i<5; i++)
+    for (int i=0; i<N; i++)
         printf("%d\t", b[i]);
 
     printf("\n");
