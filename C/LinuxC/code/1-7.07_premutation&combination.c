bfab61ba4a800031aa04fa06a5f04f0e57dd3242
wei <t104306033@ntut.org.tw>
2019-12-08 21:14:25 +0800

    feat(linuxc/code): waiting for learn gdb

--- C/LinuxC/code/1-7.07_premutation&combination.c
+++ C/LinuxC/code/1-7.07_premutation&combination.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 
-// #define N 6
+#define N 3
+int a[N] = { 1, 2, 3 };
+// #define arraySize(array) (sizeof(array)/sizeof(*array))
 
-#define arraySize(array) (sizeof(array)/sizeof(*array))
+void print_array (void) {
+    for (int i=0; i<N; i++)
+        printf("%d  ", a[i]);
+    printf("\n");
+}
 
-void print_array (int *array, int arrayLength) {
-    for (int i=0; i<arrayLength; i++)
-        printf("%d  ", array[i]);
-    printf("\n"); 
+void swap (int x, int y) {
+    int temp = 0;
+    temp = a[x];
+    a[x] = a[y];
+    a[y] = temp;
+}
+
+void prem (int offset) {
+    if (offset == N-1){
+        print_array();
+        return;
+    }
+    for (int i=offset; i<N; i++) {
+        swap(i, offset);
+        prem(offset + 1);
+        swap(i, offset);
+    } 
 }
 
 
 int main (void) {
-
-    int a[] = { 1, 2, 3, 4, 5, 6 };
     
-    int arrayLength = arraySize(a);
-    
-    print_array(a, arrayLength);
+    prem(0);
 
     return 0;
 }
